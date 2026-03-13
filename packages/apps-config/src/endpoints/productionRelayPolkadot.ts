925e4660a907d2b77a4d25003203c7b7c66daae4
kukabi <2969501+kukabi@users.noreply.github.com>
2026-03-13 15:46:34 +0000

    chore: update endpoint availability

--- packages/apps-config/src/endpoints/productionRelayPolkadot.ts
+++ packages/apps-config/src/endpoints/productionRelayPolkadot.ts
@@ -42,7 +42,7 @@ export const prodParasPolkadot: Omit<EndpointOption, 'teleport'>[] = [
         url: 'wss://acala-rpc-3.aca-api.network/ws'
       },
       {
-        isAvailable: true,
+        isAvailable: false,
         name: 'Dwellir',
         url: 'wss://acala-rpc.n.dwellir.com'
       },
@@ -616,7 +616,7 @@ export const prodParasPolkadot: Omit<EndpointOption, 'teleport'>[] = [
         url: 'wss://rpc.hydradx.cloud'
       },
       {
-        isAvailable: true,
+        isAvailable: false,
         name: 'Helikon',
         url: 'wss://rpc.helikon.io/hydradx'
       },
