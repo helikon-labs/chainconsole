5458a8d97374f5d19a5d8457cd8607173c79773c
kukabi <kutsalbilgin@gmail.com>
2026-03-17 11:02:14 +0300

    Enable Curio endpoint.

--- packages/apps-config/src/endpoints/productionRelayKusama.ts
+++ packages/apps-config/src/endpoints/productionRelayKusama.ts
@@ -198,7 +198,7 @@ export const prodParasKusama: Omit<EndpointOption, 'teleport'>[] = [
     info: 'curio',
     paraId: 3339,
     providers: [{
-      isAvailable: false,
+      isAvailable: true,
       name: 'Curio',
       url: 'wss://parachain.curioinvest.com/'
     }],
