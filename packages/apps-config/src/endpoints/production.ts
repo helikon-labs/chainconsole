f1c8542afeaa9dc3bb9ccf23371bc2ba26ef2763
kukabi <2969501+kukabi@users.noreply.github.com>
2026-04-01 08:22:13 +0000

    chore: update endpoint availability

--- packages/apps-config/src/endpoints/production.ts
+++ packages/apps-config/src/endpoints/production.ts
@@ -135,7 +135,7 @@ export const prodChains: Omit<EndpointOption, 'teleport'>[] = [
       name: 'Latent Holdings (Lite)',
       url: 'wss://lite.sub.latent.to:443'
     }, {
-      isAvailable: true,
+      isAvailable: false,
       name: 'OnFinality (Archive)',
       url: 'wss://bittensor-finney.api.onfinality.io/public-ws'
     }, {
