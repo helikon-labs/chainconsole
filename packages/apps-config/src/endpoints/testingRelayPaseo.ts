27b745bfd6fa4506844834d6fe150e90d1236ab5
kukabi <2969501+kukabi@users.noreply.github.com>
2026-05-13 17:21:51 +0000

    chore: update endpoint availability

--- packages/apps-config/src/endpoints/testingRelayPaseo.ts
+++ packages/apps-config/src/endpoints/testingRelayPaseo.ts
@@ -715,7 +715,7 @@ export const testRelayPaseo: EndpointOption = {
     name: 'IBP1',
     url: 'wss://paseo.ibp.network'
   }, {
-    isAvailable: true,
+    isAvailable: false,
     name: 'IBP2',
     url: 'wss://paseo.dotters.network'
   }, {
