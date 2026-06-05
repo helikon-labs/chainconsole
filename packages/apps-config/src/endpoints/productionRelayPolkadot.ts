9a321aec18026e49fb10e1bd556ddce5c02a8ad2
kukabi <2969501+kukabi@users.noreply.github.com>
2026-06-05 10:55:51 +0000

    chore: update endpoint availability

--- packages/apps-config/src/endpoints/productionRelayPolkadot.ts
+++ packages/apps-config/src/endpoints/productionRelayPolkadot.ts
@@ -42,7 +42,7 @@ export const prodParasPolkadot: Omit<EndpointOption, 'teleport'>[] = [
         url: 'wss://acala-rpc-3.aca-api.network/ws'
       },
       {
-        isAvailable: true,
+        isAvailable: false,
         name: 'Dwellir',
         url: 'wss://acala-rpc.n.dwellir.com'
       },
@@ -1304,7 +1304,7 @@ export const prodParasPolkadot: Omit<EndpointOption, 'teleport'>[] = [
     paraId: 3417,
     providers: [
       {
-        isAvailable: false,
+        isAvailable: true,
         name: 'RakSonCebu',
         url: 'wss://rakson-ceb-rpc.xode.net'
       },
@@ -1421,7 +1421,7 @@ export const prodParasPolkadotCommon: EndpointOption[] = [
         url: 'wss://bridge-hub-polkadot-rpc.n.dwellir.com'
       },
       {
-        isAvailable: true,
+        isAvailable: false,
         name: 'IBP1',
         url: 'wss://bridge-hub-polkadot.ibp.network'
       },
@@ -1504,7 +1504,7 @@ export const prodParasPolkadotCommon: EndpointOption[] = [
         url: 'wss://polkadot-collectives-rpc.polkadot.io'
       },
       {
-        isAvailable: true,
+        isAvailable: false,
         name: 'RadiumBlock',
         url: 'wss://collectives.public.curie.radiumblock.co/ws'
       },
