64e51ab962d2b976b46a511c3bfdc7039a17b85f
kukabi <2969501+kukabi@users.noreply.github.com>
2026-08-15 07:40:12 +0000

    chore: update endpoint availability

--- packages/apps-config/src/endpoints/productionRelayPolkadot.ts
+++ packages/apps-config/src/endpoints/productionRelayPolkadot.ts
@@ -42,7 +42,7 @@ export const prodParasPolkadot: Omit<EndpointOption, 'teleport'>[] = [
         url: 'wss://acala-rpc-3.aca-api.network/ws'
       },
       {
-        isAvailable: true,
+        isAvailable: false,
         name: 'Dwellir',
         url: 'wss://acala-rpc.n.dwellir.com'
       },
@@ -663,7 +663,7 @@ export const prodParasPolkadot: Omit<EndpointOption, 'teleport'>[] = [
         url: 'wss://nexus.dotters.network'
       },
       {
-        isAvailable: false,
+        isAvailable: true,
         name: 'Polytope Labs',
         url: 'wss://nexus.rpc.polytope.technology'
       }
@@ -875,7 +875,7 @@ export const prodParasPolkadot: Omit<EndpointOption, 'teleport'>[] = [
     paraId: 2004,
     providers: [
       {
-        isAvailable: true,
+        isAvailable: false,
         name: 'Allnodes',
         url: 'wss://moonbeam-rpc.publicnode.com'
       },
@@ -920,7 +920,7 @@ export const prodParasPolkadot: Omit<EndpointOption, 'teleport'>[] = [
         url: 'wss://moonbeam.public.curie.radiumblock.co/ws'
       },
       {
-        isAvailable: false,
+        isAvailable: true,
         name: 'UnitedBloc',
         url: 'wss://moonbeam.unitedbloc.com'
       }
